c420426abad03b3bad8a468c7706d828c596ed69
RafaelGasparoto <rafaelgasparoto4@gmail.com>
2025-09-24 22:42:12 -0300

    Endpoint /assinatura/assinar
    Acesso ao recurso de assinatura de documento
    Entrada deve conter um documento
    Retorno é o mesmo documento porém assinado

---
+++ src/main/java/br/edu/utfpr/td/tsi/assinador/utf/controlador/AssinaturaControlador.java
@@ -0,0 +1,30 @@
+package br.edu.utfpr.td.tsi.assinador.utf.controlador;
+
+import br.edu.utfpr.td.tsi.assinador.utf.servico.AssinaturaServico;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/assinatura")
+public class AssinaturaControlador {
+
+	@Autowired
+	AssinaturaServico assinaturaServico;
+
+	@PostMapping(consumes = "multipart/form-data", path = "assinar")
+	public ResponseEntity<?> assinarDocumento(@RequestParam("file") MultipartFile file) {
+		byte[] pdfAssinado = assinaturaServico.assinarDocumento(file);
+
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=documento_assinado.pdf")
+				.contentType(MediaType.APPLICATION_PDF).body(pdfAssinado);
+	}
+}
