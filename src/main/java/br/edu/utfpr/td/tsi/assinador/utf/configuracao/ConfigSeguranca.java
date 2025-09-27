888bcac6622b56d0b794e733bce1bb2393507c70
RafaelGasparoto <rafaelgasparoto4@gmail.com>
2025-09-27 12:27:04 -0300

    Valida o token para rotas que exigem segurança

--- src/main/java/br/edu/utfpr/td/tsi/assinador/utf/configuracao/ConfigSeguranca.java
+++ src/main/java/br/edu/utfpr/td/tsi/assinador/utf/configuracao/ConfigSeguranca.java
@@ -1,23 +1,34 @@
 package br.edu.utfpr.td.tsi.assinador.utf.configuracao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.edu.utfpr.td.tsi.assinador.utf.seguranca.JwtFiltro;
+import br.edu.utfpr.td.tsi.assinador.utf.tratamento.JwtAutenticacaoExcecao;
 
 @Configuration
 @EnableWebSecurity
 public class ConfigSeguranca {
 
+	@Autowired
+	private JwtAutenticacaoExcecao jwtEntryPoint;
+
+	@Autowired
+	private JwtFiltro jwtFiltro;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
-		.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
-                .requestMatchers("/autenticacao/**").permitAll()
-				.anyRequest().authenticated() // Todas as outras rotas exigem autenticação
-		);
+				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+						.requestMatchers("/autenticacao/**").permitAll().anyRequest().authenticated())
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(jwtEntryPoint))
+				.addFilterBefore(jwtFiltro, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
